788cdbeaa792292d675cc2b24c9ea783aaab6656
franksharpe <145439826+franksharpe@users.noreply.github.com>
2023-10-13 11:29:51 +0100

    Add files via upload

--- JS/test.js
+++ JS/test.js
@@ -7,3 +7,20 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.modal-content.email.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.modal-content.email.focus();
+return false;
+}
+}
