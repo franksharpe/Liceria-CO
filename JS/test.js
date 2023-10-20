a938d45c2c9d578f0e7e0754e291c343971bcead
franksharpe <145439826+franksharpe@users.noreply.github.com>
2023-10-20 10:26:52 +0100

    Add files via upload

--- JS/test.js
+++ JS/test.js
@@ -1,7 +1,7 @@
-// Get the modal
+// show the sign up page 
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -9,31 +9,39 @@ window.onclick = function(event) {
 }
 
 function validateform(){  
+  
+  // var name
   var name=document.myform.email.value;  
+
+  //var passwords
   var password=document.myform.psw.value;
+
+  //var conf password
+  var confirmPassword = document.myform['psw-repeat'].value;
     
-  if (password.length<2){  
-    alert("Name can't be shorter than 2 letters ");  
-    return false;  
-  }else if(password.length<6){  
-    alert("Password must be at least 6 characters long.");  
+  //check name length 
+  if (name.length<5){  
+    alert("Name can't be shorter than 5 letters. ");  
     return false;  
     }  
+
+    //check password length
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters long.");
+      return false;
+    }
+  
+    //check passwords match 
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return false;
+    }
 
      // Display a message with the registered name
      alert (name + " thanks for signing up ") ;
      window.location.href = "test.html";
-  }  
-  var check = function() {
-    if (document.getElementById('psw').value ==
-      document.getElementById('psw-repeat').value) {
-      document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching passwords';
-    } else {
-      document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching passwords';
-    }
-  }
+ 
+    }  
 
   
   
